819c57bc3265bbb94aa1078c7ea05b7ecc9c6524
StokesDylan <stokes0803@yahoo.com>
2025-04-19 18:35:44 -0400

    convert gyro data into roll angles

--- Sensor_Data/Sensor_Data/Sensor_Data.ino
+++ Sensor_Data/Sensor_Data/Sensor_Data.ino
@@ -29,6 +29,10 @@ VL53L4CD tof4(&DEV_I2C, 9);
 int data[10];
 int filtered_data[10];
 
+unsigned long previousMillis = 0;  // store last time action was taken
+const long interval = 30;          // interval at which to do something (ms)
+unsigned long CurrentMillis = 0;
+
 void setup() {
   // Initialize serial for output.
   SerialPort.begin(9600);
@@ -97,116 +101,121 @@ void setup() {
 }
 
 void loop() {
-  uint8_t NewDataReady = 0;
-  VL53L4CD_Result_t results;
-  uint8_t status;
-  char report[64];
+  currentMillis = millis();
+
+  if (currentMillis - previousMillis >= interval) {
+    previousMillis = currentMillis;
 
 
-  do {
-    status = tof1.VL53L4CD_CheckForDataReady(&NewDataReady);
-  } while (!NewDataReady);
+    uint8_t NewDataReady = 0;
+    VL53L4CD_Result_t results;
+    uint8_t status;
+    char report[64];
 
-  if ((!status) && (NewDataReady != 0)) {
-    // (Mandatory) Clear HW interrupt to restart measurements
-    tof1.VL53L4CD_ClearInterrupt();
 
-    // Read measured distance and saves in data
-    tof1.VL53L4CD_GetResult(&results);
-    data[0] = results.distance_mm;
-  }
+    do {
+      status = tof1.VL53L4CD_CheckForDataReady(&NewDataReady);
+    } while (!NewDataReady);
 
-  uint8_t NewDataReady2 = 0;
-  VL53L4CD_Result_t results2;
-  uint8_t status2;
-  char report2[64];
+    if ((!status) && (NewDataReady != 0)) {
+      // (Mandatory) Clear HW interrupt to restart measurements
+      tof1.VL53L4CD_ClearInterrupt();
 
-  do {
-    status2 = tof2.VL53L4CD_CheckForDataReady(&NewDataReady2);
-  } while (!NewDataReady2);
-
-  if ((!status2) && (NewDataReady2 != 0)) {
-    // (Mandatory) Clear HW interrupt to restart measurements
-    tof2.VL53L4CD_ClearInterrupt();
-
-    // Read measured distance and saves in data
-    tof2.VL53L4CD_GetResult(&results2);
-    data[1] = results2.distance_mm;
-  }
-
-  uint8_t NewDataReady3 = 0;
-  VL53L4CD_Result_t results3;
-  uint8_t status3;
-  char report3[64];
-
-  do {
-    status3 = tof3.VL53L4CD_CheckForDataReady(&NewDataReady3);
-  } while (!NewDataReady3);
-
-  if ((!status3) && (NewDataReady3 != 0)) {
-    // (Mandatory) Clear HW interrupt to restart measurements
-    tof3.VL53L4CD_ClearInterrupt();
-
-    // Read measured distance and saves in data
-    tof3.VL53L4CD_GetResult(&results3);
-    data[2] = results3.distance_mm;
-  }
-
-  uint8_t NewDataReady4 = 0;
-  VL53L4CD_Result_t results4;
-  uint8_t status4;
-  char report4[64];
-
-  do {
-    status4 = tof4.VL53L4CD_CheckForDataReady(&NewDataReady4);
-  } while (!NewDataReady4);
-
-  if ((!status4) && (NewDataReady4 != 0)) {
-    // (Mandatory) Clear HW interrupt to restart measurements
-    tof4.VL53L4CD_ClearInterrupt();
-
-    // Read measured distance and saves in data
-    tof4.VL53L4CD_GetResult(&results4);
-    data[3] = results4.distance_mm;
-  }
-
-  lsm.read(); /* ask it to read in the data */
-
-  /* Get a new sensor event */
-  sensors_event_t a, g, m, t;
-
-  lsm.getEvent(&a, &g, &m, &t);
-  // saves IMU values in data, accel in m/s^2, gyro in rad/s
-  data[4] = a.acceleration.x;
-  data[5] = a.acceleration.y;
-  data[6] = a.acceleration.z;
-  data[7] = g.gyro.x;
-  data[8] = g.gyro.y;
-  data[9] = g.gyro.z;
-
-  for(int i=0; i<10; i++){
-    if(i < 4){
-    filtered_data[i] = (0.4)*data[i] + (1-0.4)*filtered_data[i];       
-    } else {
-      filtered_data[i] = (0.9)*data[i] + (1-0.9)*filtered_data[i];
-      //filtered_data[i] = data[i];
+      // Read measured distance and saves in data
+      tof1.VL53L4CD_GetResult(&results);
+      data[0] = results.distance_mm;
     }
- 
+
+    uint8_t NewDataReady2 = 0;
+    VL53L4CD_Result_t results2;
+    uint8_t status2;
+    char report2[64];
+
+    do {
+      status2 = tof2.VL53L4CD_CheckForDataReady(&NewDataReady2);
+    } while (!NewDataReady2);
+
+    if ((!status2) && (NewDataReady2 != 0)) {
+      // (Mandatory) Clear HW interrupt to restart measurements
+      tof2.VL53L4CD_ClearInterrupt();
+
+      // Read measured distance and saves in data
+      tof2.VL53L4CD_GetResult(&results2);
+      data[1] = results2.distance_mm;
+    }
+
+    uint8_t NewDataReady3 = 0;
+    VL53L4CD_Result_t results3;
+    uint8_t status3;
+    char report3[64];
+
+    do {
+      status3 = tof3.VL53L4CD_CheckForDataReady(&NewDataReady3);
+    } while (!NewDataReady3);
+
+    if ((!status3) && (NewDataReady3 != 0)) {
+      // (Mandatory) Clear HW interrupt to restart measurements
+      tof3.VL53L4CD_ClearInterrupt();
+
+      // Read measured distance and saves in data
+      tof3.VL53L4CD_GetResult(&results3);
+      data[2] = results3.distance_mm;
+    }
+
+    uint8_t NewDataReady4 = 0;
+    VL53L4CD_Result_t results4;
+    uint8_t status4;
+    char report4[64];
+
+    do {
+      status4 = tof4.VL53L4CD_CheckForDataReady(&NewDataReady4);
+    } while (!NewDataReady4);
+
+    if ((!status4) && (NewDataReady4 != 0)) {
+      // (Mandatory) Clear HW interrupt to restart measurements
+      tof4.VL53L4CD_ClearInterrupt();
+
+      // Read measured distance and saves in data
+      tof4.VL53L4CD_GetResult(&results4);
+      data[3] = results4.distance_mm;
+    }
+
+    lsm.read(); /* ask it to read in the data */
+
+    /* Get a new sensor event */
+    sensors_event_t a, g, m, t;
+
+    lsm.getEvent(&a, &g, &m, &t);
+    // saves IMU values in data, accel in m/s^2, gyro in rad/s
+    data[4] = a.acceleration.x;
+    data[5] = a.acceleration.y;
+    data[6] = a.acceleration.z;
+    data[7] += g.gyro.x * interval;
+    data[8] += g.gyro.y * interval;
+    data[9] += g.gyro.z * interval;
+
+    for (int i = 0; i < 10; i++) {
+      if (i < 4) {
+        filtered_data[i] = (0.4) * data[i] + (1 - 0.4) * filtered_data[i];
+      } else {
+        filtered_data[i] = (0.9) * data[i] + (1 - 0.9) * filtered_data[i];
+        //filtered_data[i] = data[i];
+      }
+    }
+
+    for (int i = 0; i < 10; i++) {
+      Serial.print(filtered_data[i]);
+      Serial.print(" ");
+    }
+    Serial.println();
   }
 
-  for(int i=0; i<10; i++){
-    Serial.print(filtered_data[i]);
-    Serial.print(" ");    
+  // function to setup resolution for IMU
+  void setupSensor() {
+    // 1.) Set the accelerometer range
+    lsm.setupAccel(lsm.LSM9DS1_ACCELRANGE_2G, lsm.LSM9DS1_ACCELDATARATE_10HZ);
+
+    // 3.) Setup the gyroscope
+    lsm.setupGyro(lsm.LSM9DS1_GYROSCALE_245DPS);
   }
-  Serial.println();
-  delay(500);
 }
-
-// function to setup resolution for IMU
-void setupSensor() {
-  // 1.) Set the accelerometer range
-  lsm.setupAccel(lsm.LSM9DS1_ACCELRANGE_2G, lsm.LSM9DS1_ACCELDATARATE_10HZ);
-
-  // 3.) Setup the gyroscope
-  lsm.setupGyro(lsm.LSM9DS1_GYROSCALE_245DPS);
-}
